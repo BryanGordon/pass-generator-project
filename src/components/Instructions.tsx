75c7215ea01f1a01e842a47fa0128b7dc8384eec
Bryan Gordon <bryanbge92@gmail.com>
2024-09-13 18:38:18 -0500

    Update component

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,7 +1,18 @@
 export function Instructions () {
   return (
-    <article>
-      Instructions to generate a password
+    <article className='bg-blue-400 p-4 pb-20 mx-8 mt-5 rounded-lg'>
+      <h1 className=' w-1/2 my-0 mx-auto text-h2 text-center'>Generation instructions</h1>
+
+      <div>
+        <h4 className='text-h4'>Enable uppercase</h4>
+        <input type='checkbox' />
+        <h4 className='text-h4'>Enable lowercase</h4>
+        <input type='checkbox' />
+        <h4 className='text-h4'>Enable numbers</h4>
+        <input type='checkbox' />
+        <h4 className='text-h4'>Enable special characters</h4>
+        <input type='checkbox' />
+      </div>
     </article>
   )
 }
