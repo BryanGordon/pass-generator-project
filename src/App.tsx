d7753f2543508d5c9828d7a8e27cc7d8cbcdb66b
Bryan Gordon <bryanbge92@gmail.com>
2024-09-13 18:37:29 -0500

    Add component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { Instructions } from './components/Instructions'
+import { Results } from './components/Results'
 
 function App () {
   return (
     <div className='container'>
       <Header />
-      <main className='bg-blue-400 p-4 min-h-full'>
+      <main>
         <Instructions />
+        <Results />
       </main>
       <Footer />
     </div>
