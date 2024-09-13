22b4e1895d43341b3d12e16214f0f337bd734235
Bryan Gordon <bryanbge92@gmail.com>
2024-09-13 18:37:57 -0500

    Update inline styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export function Header () {
   return (
-    <header className='bg-purple-800 w-full'>
-      <h1 className='my-0 mx-auto w-1/2 text-center'>Header</h1>
+    <header className='bg-green-400 w-full'>
+      <h1 className=' text-h1 my-0 mx-auto w-1/2 text-center p-7'>Password generator</h1>
     </header>
   )
 }
