be84e5e57b3fae07ea94519c9c23bbe8d02163fb
Bryan Gordon <bryanbge92@gmail.com>
2024-09-16 18:40:13 -0500

    Add structure component
    - Add styles

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,18 +1,27 @@
 export function Instructions () {
   return (
     <article className='bg-blue-400 p-4 pb-20 mx-8 mt-5 rounded-lg'>
-      <h1 className=' w-1/2 my-0 mx-auto text-h2 text-center'>Generation instructions</h1>
+      <h2 className='text-h2 text-center'>Generation instructions</h2>
 
-      <div>
-        <h4 className='text-h4'>Enable uppercase</h4>
-        <input type='checkbox' />
-        <h4 className='text-h4'>Enable lowercase</h4>
-        <input type='checkbox' />
-        <h4 className='text-h4'>Enable numbers</h4>
-        <input type='checkbox' />
-        <h4 className='text-h4'>Enable special characters</h4>
-        <input type='checkbox' />
-      </div>
+      <form className='flex flex-row justify-around pt-12'>
+        <div className='flex flex-col items-center'>
+          <h4 className='text-h4 content-center'>Enable uppercase</h4>
+          <input id='raw' type='checkbox' className='w-[19px] h-[19px] mt-3' />
+        </div>
+        <div className='flex flex-col items-center'>
+          <h4 className='text-h4'>Enable lowercase</h4>
+          <input type='checkbox' className='w-[19px] h-[19px] mt-3' />
+        </div>
+        <div className='flex flex-col items-center'>
+          <h4 className='text-h4'>Enable numbers</h4>
+          <input type='checkbox' className='w-[19px] h-[19px] mt-3' />
+        </div>
+        <div className='flex flex-col items-center'>
+          <h4 className='text-h4'>Enable special characters</h4>
+          <input type='checkbox' className='w-[19px] h-[19px] mt-3' />
+        </div>
+
+      </form>
     </article>
   )
 }
