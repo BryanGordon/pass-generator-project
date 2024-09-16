df9e7c233efe95b0fcdb3d17b709ed1662fecd30
Bryan Gordon <bryanbge92@gmail.com>
2024-09-16 18:40:50 -0500

    Add structure component
    - Add styles

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,7 +1,10 @@
 export function Results () {
   return (
-    <article className='bg-blue-400 p-4 pb-20 mx-8 mt-5 rounded-lg text-center'>
-      Resutados
+    <article className='bg-blue-400 p-4 pb-20 mx-8 mt-5 rounded-lg'>
+      <h2 className='text-h2 text-center'>Your new password</h2>
+      <div className='flex justify-center'>
+        <input type='text' className='mt-3 rounded-xl h-18 items-center' />
+      </div>
     </article>
   )
 }
