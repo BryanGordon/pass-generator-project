8312b31e407d54468ba6d4e5decf97f24fb4a29c
Bryan Gordon <bryanbge92@gmail.com>
2024-09-17 19:30:12 -0500

    Update component structure

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,25 +1,40 @@
 export function Instructions () {
   return (
-    <article className='bg-blue-400 p-4 pb-20 mx-8 mt-5 rounded-lg'>
+    <article className='bg-blue-400 p-4 pb-14 mx-8 mt-5 rounded-lg'>
       <h2 className='text-h2 text-center'>Generation instructions</h2>
 
-      <form className='flex flex-row justify-around pt-12'>
-        <div className='flex flex-col items-center'>
-          <h4 className='text-h4 content-center'>Enable uppercase</h4>
-          <input id='raw' type='checkbox' className='w-[19px] h-[19px] mt-3' />
-        </div>
-        <div className='flex flex-col items-center'>
-          <h4 className='text-h4'>Enable lowercase</h4>
-          <input type='checkbox' className='w-[19px] h-[19px] mt-3' />
-        </div>
-        <div className='flex flex-col items-center'>
-          <h4 className='text-h4'>Enable numbers</h4>
-          <input type='checkbox' className='w-[19px] h-[19px] mt-3' />
-        </div>
-        <div className='flex flex-col items-center'>
-          <h4 className='text-h4'>Enable special characters</h4>
-          <input type='checkbox' className='w-[19px] h-[19px] mt-3' />
-        </div>
+      <form className='pt-12'>
+        <section className='flex flex-row justify-around'>
+
+          <div className='flex flex-col items-center'>
+            <h4 className='text-h4 content-center'>Enable uppercase</h4>
+            <input type='checkbox' className='w-[19px] h-[19px] mt-3' />
+          </div>
+          <div className='flex flex-col items-center'>
+            <h4 className='text-h4'>Enable lowercase</h4>
+            <input type='checkbox' className='w-[19px] h-[19px] mt-3' />
+          </div>
+          <div className='flex flex-col items-center'>
+            <h4 className='text-h4'>Enable numbers</h4>
+            <input type='checkbox' className='w-[19px] h-[19px] mt-3' />
+          </div>
+          <div className='flex flex-col items-center'>
+            <h4 className='text-h4'>Enable special characters</h4>
+            <input type='checkbox' className='w-[19px] h-[19px] mt-3' />
+          </div>
+
+        </section>
+
+        <section>
+
+          <div className='pt-12'>
+            <h3 className='text-h3 text-center'>Password lenght</h3>
+            <div className='w-1/2 my-0 mx-auto text-center pt-5'>
+              Slider
+            </div>
+          </div>
+
+        </section>
 
       </form>
     </article>
