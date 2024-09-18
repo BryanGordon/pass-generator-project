d29c63b17ae5c75fa51b9cec00cacbfd71d6ba7b
Bryan Gordon <bryanbge92@gmail.com>
2024-09-17 19:29:45 -0500

    Add shadcn to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ['class'],
+    content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {
-      fontSize: {
-        'h1': '32px',
-        'h2': '24px',
-        'h3': '18.72px',
-        'h4': '16px',
-        'h5': '13.28px',
-        'h6': '10.72px'
-      }
-    }
+  	extend: {
+  		fontSize: {
+  			h1: '32px',
+  			h2: '24px',
+  			h3: '18.72px',
+  			h4: '16px',
+  			h5: '13.28px',
+  			h6: '10.72px'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: []
+  plugins: [require("tailwindcss-animate")]
 }
