644e987e27ccb5122531e0cf73d5043d570bfd66
Bryan Gordon <bryanbge92@gmail.com>
2024-09-17 19:30:27 -0500

    Update styles

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -2,7 +2,7 @@ export function Results () {
   return (
     <article className='bg-blue-400 p-4 pb-20 mx-8 mt-5 rounded-lg'>
       <h2 className='text-h2 text-center'>Your new password</h2>
-      <div className='flex justify-center'>
+      <div className='flex justify-center pt-2'>
         <input type='text' className='mt-3 rounded-xl h-18 items-center' />
       </div>
     </article>
