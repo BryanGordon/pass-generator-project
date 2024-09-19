5dae5e4b5faba5122f26d8d87d37dcb719692c82
Bryan Gordon <bryanbge92@gmail.com>
2024-09-18 23:26:52 -0500

    Add styles

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,8 @@
 export function Results () {
+  const random = Math.random().toString(36)
+  console.log(random)
   return (
-    <article className='bg-blue-400 p-4 pb-20 mx-8 mt-5 rounded-lg'>
+    <article className='bg-blue-400 p-4 pb-20 mx-8 mt-5 mb-5 rounded-lg'>
       <h2 className='text-h2 text-center'>Your new password</h2>
       <div className='flex justify-center pt-2'>
         <input type='text' className='mt-3 rounded-xl h-18 items-center' />
