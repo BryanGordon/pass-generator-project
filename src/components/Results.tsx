c73cc216503f3ce33d3d813ab549923b51e7b851
Bryan Gordon <bryanbge92@gmail.com>
2024-09-20 18:46:31 -0500

    Update component
    - Implement logic
    - Update card styles

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,16 +1,27 @@
 export function Results () {
-  const random = Math.random().toString(10)
-  const aux = Math.random().toString(36).toUpperCase()
   const letters = 'abcdefgh'
+  const number = '123'
   const specials = '!@#$'
-  console.log('random variable  ' + random)
-  console.log('aux variable  ' + aux)
+  const length = 10
+  const password = letters + number + specials
+
+  const render = () => {
+    let aux = ''
+    let items = ''
+    for (let x = 0; x < length; x++) {
+      items = Math.floor(Math.random() * password.length)
+      aux += password.charAt(items)
+    }
+    return aux
+  }
+
+  console.log('random function  ' + render())
 
   return (
-    <article className='bg-blue-400 p-4 pb-20 mx-8 mt-5 mb-5 rounded-lg'>
+    <article className='bg-blue-400 p-10 pb-10 mx-80 mt-5 mb-5 rounded-lg'>
       <h2 className='text-h2 text-center'>Your new password</h2>
       <div className='flex justify-center pt-2'>
-        <input type='text' className='mt-3 rounded-xl h-18 items-center' />
+        <input value={render()} type='text' className='mt-3 rounded-xl h-18 items-center text-center' />
       </div>
     </article>
   )
