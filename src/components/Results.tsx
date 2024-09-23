483eaf62b93c51842cd105806daea07a4b195faf
Bryan Gordon <bryanbge92@gmail.com>
2024-09-23 14:02:16 -0500

    Factorized component

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,27 +1,19 @@
-export function Results () {
-  const letters = 'abcdefgh'
-  const number = '123'
-  const specials = '!@#$'
-  const length = 10
-  const password = letters + number + specials
+import { Generator } from '@/logic/generator'
 
-  const render = () => {
-    let aux = ''
-    let items = ''
-    for (let x = 0; x < length; x++) {
-      items = Math.floor(Math.random() * password.length)
-      aux += password.charAt(items)
-    }
-    return aux
+export function Results () {
+  const info = {
+    letters: 'abcdefgh',
+    numbers: '123',
+    specials: '!@#$'
   }
 
-  console.log('random function  ' + render())
+  const data = Generator(info)
 
   return (
     <article className='bg-blue-400 p-10 pb-10 mx-80 mt-5 mb-5 rounded-lg'>
       <h2 className='text-h2 text-center'>Your new password</h2>
       <div className='flex justify-center pt-2'>
-        <input value={render()} type='text' className='mt-3 rounded-xl h-18 items-center text-center' />
+        <input value={data} type='text' className='mt-3 rounded-xl h-18 items-center text-center' />
       </div>
     </article>
   )
