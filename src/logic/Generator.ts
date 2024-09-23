5399b3fc93660739998abd16ad3f35fb7ed26505
Bryan Gordon <bryanbge92@gmail.com>
2024-09-23 14:02:42 -0500

    Create generator logic file

---
+++ src/logic/Generator.ts
@@ -0,0 +1,14 @@
+import { type Data } from '@/types/types'
+const length = 5
+
+export function Generator ({ letters, numbers, specials }: Data) {
+  let aux:string = ''
+  let items:number = 0
+  const password = letters + numbers + specials
+
+  for (let x = 0; x < length; x++) {
+    items = Math.floor(Math.random() * password.length)
+    aux += password.charAt(items)
+  }
+  return aux
+}
